e954d6af22166a68ae31fda482a5fd0b53815d3b
Abhishek <abhigeu9193@gmail.com>
2022-04-27 23:50:46 +0530

    Get No between 1 to 6

---
+++ SnakeLadderPoblem/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+class SnakeLadderProblem
+{
+    public static object rollDice;
+    public static object random;
+    int playPos = 0;
+    static void Main(string[] args)
+    {
+       
+        Random random = new Random();
+        int rollDice = random.Next(1, 7);
+        Console.WriteLine("Rolled Dice number is :" +rollDice);
+    }
+}
