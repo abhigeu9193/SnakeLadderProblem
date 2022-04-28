a7e684a3f5f4005a5163527961929c63751a0cc0
Abhishek <abhigeu9193@gmail.com>
2022-04-28 22:49:11 +0530

    snakeproblem-1

---
+++ SnakeLader-UC-1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int playerPosition = 0;
+Random rm2 = new Random();
+int playerMove = rm2.Next(1, 7);
