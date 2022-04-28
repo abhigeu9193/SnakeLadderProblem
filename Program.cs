f89838a40ad51acf8bc87bd16c0a1d383b66cad6
Abhishek kumar singh <82291395+abhigeu9193@users.noreply.github.com>
2022-04-28 22:56:02 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-using System;
-class SnakeLadderProblem
-{
-    //public static object rollDice;
-    //public static object random;
-    public const int IS_PLAYER_POSITION = 0;
-    public const int IS_SNAKE_POSITION = 1;
-
-    int playPos = 0;
-    static void Main(string[] args)
-    {
-        int player = 0;
-        Random random = new();
-        int rollDice = random.Next(0, 7);
-        switch (rollDice)
-        {
-            case IS_PLAYER_POSITION:
-                player += rollDice;
-                break;
-            case IS_SNAKE_POSITION:
-                player -= rollDice;
-                break;
-            default:
-                player = 0;
-                break;
-        }
-        Console.WriteLine("Rolled Dice number is :" + rollDice);
-    }
-}
-
+int playerPosition = 0;
+Random rm2 = new Random();
+int playerMove = rm2.Next(1, 7);
